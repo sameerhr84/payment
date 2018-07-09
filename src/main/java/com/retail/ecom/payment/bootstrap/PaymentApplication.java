b05847d9c734f8fe353b4ec3e42d7d6cee5bcf22
sameer <sameer920@gmail.com>
2018-07-08 20:36:26 -0500

    payment

--- src/main/java/com/retail/ecom/payment/bootstrap/PaymentApplication.java
+++ src/main/java/com/retail/ecom/payment/bootstrap/PaymentApplication.java
@@ -1,4 +1,4 @@
-package com.retail.ecom.content.bootstrap;
+package com.retail.ecom.payment.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
