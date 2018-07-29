adc497555a074b2fe0db82df2758002644cb87eb
sameer <sameer920@gmail.com>
2018-07-29 14:41:47 -0500

    eureka integration

--- src/main/java/com/retail/ecom/payment/controller/PaymentController.java
+++ src/main/java/com/retail/ecom/payment/controller/PaymentController.java
@@ -1,7 +1,5 @@
 package com.retail.ecom.payment.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,17 +14,17 @@ public class PaymentController {
 	
 	
 	@RequestMapping("/createpayment")
-    public  ResponseEntity<?>  createpayment(@RequestBody Payment payment) {	
+    public  Payment  createpayment(@RequestBody Payment payment) {	
 		payment.setId(PaymentApplication.paymentId++);
 		PaymentApplication.payments.put(payment.getId(), payment);
-		return new ResponseEntity<Payment>(payment, HttpStatus.OK);
+		return payment;
     }
 	
 	@RequestMapping("/authorizepayment")
-    public  ResponseEntity<?>  authorizepayment(@RequestParam(value="id") String id) {
+    public  Payment  authorizepayment(@RequestParam(value="id") String id) {
 		Payment payment=PaymentApplication.payments.get(id);
 		payment.setAuthId(payment.getId()*12+"");
-		return new ResponseEntity<Payment>(payment, HttpStatus.OK);
+		return payment;
     }
 
 }
